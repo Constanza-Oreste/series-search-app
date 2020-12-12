f9aa17bcd0e87182cf0fe1464a6bdafa1551f5c3
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-12-11 22:20:26 -0300

    Trying to add spinner while fetching API, to be implemented in another moment

--- src/components/SearchSeries.jsx
+++ src/components/SearchSeries.jsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import './cssComponents/SearchSeries.css'
 import Cards from './Cards'
 import 'antd/dist/antd.css'
-import { Input } from 'antd';
+import { Input, Spin, Space } from 'antd';
+
 const { Search } = Input;
 
 const SearchSeries = () => {
 
     const [query, setQuery] = useState('');
     const [series, setSeries] = useState([]);
+    //const [loading, setLoading] = useState(false);
 
     const searchSeries = async () => {
         //API Call
@@ -18,6 +20,7 @@ const SearchSeries = () => {
             const res = await fetch(url);
             const data = await res.json();
             setSeries(data);
+            //setLoading(true);
         } catch (err) {
             console.error(err);
         }
